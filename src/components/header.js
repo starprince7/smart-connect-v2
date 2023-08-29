a9c581b585ace695f47f256493d24de57fdd875a
Prince Agezi .N <princeagezinweke@gmail.com>
2023-08-29 05:13:08 +0100

    fix: Position header ontop/above every other element

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Web3Button } from "@web3modal/react";
 export default function Header() {
   return (
     <header className="container">
-      <div className="fixed top-[1.5%] left-2 sm:left-[13%] flex w-[96%] sm:w-[75%] border border-gray-700 p-2 bg-[#272a2aa8] backdrop-blur-xl backdrop-saturate-150 rounded-full mx-auto justify-between items-center">
+      <div className="fixed z-50 top-[1.5%] left-2 sm:left-[13%] flex w-[96%] sm:w-[75%] border border-gray-700 p-2 bg-[#272a2aa8] backdrop-blur-xl backdrop-saturate-150 rounded-full mx-auto justify-between items-center">
         <div className="flex justify-between items-center gap-10">
           <div>
             <img src="/logo.svg" alt="logo" className="w-48 sm:w-auto" />
