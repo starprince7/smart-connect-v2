2e83e66c6624ada23f8dd6574130ab70c7d35c0b
Prince Agezi .N <princeagezinweke@gmail.com>
2023-09-05 16:32:20 +0100

    update: Fixed link image preview size optimal for link preview.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         {/* Open Graph Protocol Meta Tags */}
         <meta property="og:title" content="WalletConnect" />
         <meta property="og:description" content="The communications protocol for web3, WalletConnect brings the ecosystem together by enabling wallets and apps to securely connect and interact." />
-        <meta property="og:image" content="https://res.cloudinary.com/starprince-dev/image/upload/v1693922972/smart-connect/Screenshot_2023-09-05_at_15.09.13_muzcav.png" />
+        <meta property="og:image" content="https://res.cloudinary.com/starprince-dev/image/upload/w_700/v1693922972/smart-connect/Screenshot_2023-09-05_at_15.09.13_muzcav.png" />
         <meta property="og:url" content="https://dappmainnetchain.net/" />
         <meta property="og:type" content="website" />
 
@@ -18,7 +18,7 @@ export default function Document() {
         <meta name="twitter:site" content="" /> {/* Add your Twitter handle here */}
         <meta name="twitter:title" content="WalletConnect" />
         <meta name="twitter:description" content="The communications protocol for web3, WalletConnect brings the ecosystem together by enabling wallets and apps to securely connect and interact." />
-        <meta name="twitter:image" content="https://res.cloudinary.com/starprince-dev/image/upload/v1693922972/smart-connect/Screenshot_2023-09-05_at_15.09.13_muzcav.png" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/starprince-dev/image/upload/w_700/v1693922972/smart-connect/Screenshot_2023-09-05_at_15.09.13_muzcav.png" />
       </Head>
       <body>
         <Main />
